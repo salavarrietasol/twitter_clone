223022f9ddd364218b49ec95c0e664696ea0bdb3
Sol <salavarrietasol@gmail.com>
2024-08-30 00:49:13 -0500

    Add profile image

--- src/Tweet.jsx
+++ src/Tweet.jsx
@@ -85,7 +85,7 @@ const Tweet = ({ tweet }) => {
                             <ul role="list" class="divide-y divide-gray-100">
                                 <li class="flex justify-between gap-x-6 py-5">
                                     <div class="flex min-w-0 gap-x-4">
-                                        <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="/img/fotoMia.jpeg" alt=""></img>
+                                        <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="./img/fotoMia.jpeg" alt="Profile Photo"></img>
                                         <div class="min-w-0 flex-auto">
                                             <p class="text-sm font-semibold leading-6 text-gray-900">Sol Salavarrieta</p>
                                             <p class="mt-1 truncate text-xs leading-5 text-gray-500">@esealatres</p>
